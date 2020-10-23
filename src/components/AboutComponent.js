07ecb1a81d406df1db17e6d8a71ff205200a43a4
Chris Kubick <socalledsound@gmail.com>
2020-10-23 14:26:46 -0700

    task3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
 function RenderPartner({partner}) {
     if (partner) {
         return (
@@ -18,13 +17,13 @@ function RenderPartner({partner}) {
     return <div />;
 }
 
-
-
 function About(props) {
 
     const partners = props.partners.map(partner => {
         return (
-            <h5>{partner.name}</h5>
+            <Media tag="li" key={partner.id}>
+                <RenderPartner partner={partner} />
+            </Media>
         );
     });
 
@@ -90,4 +89,4 @@ function About(props) {
     );
 }
 
-export default About;
+export default About
